a09626dfec97f21a6d1cb3e141150a911e952e8b
Serpent03 <dumbpilot03@gmail.com>
2024-02-25 20:04:48 +0530

    feat: serialization on arrays. converted to individual elements

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@
 * @todo Utilize XML data format to get the process done.
 */
 
+#[allow(unused)]
 use std::io::{ stdin, stdout, Write };
 
+
+fn print_to_byte(arr: &[i32], n: u32) -> () {
+    for i in 0..n {
+        print!("{}", arr[i as usize]);
+    }
+    println!();
+}
+
 fn main() {
-    let arr: [i32] = [1, 2, 3, 4, 5];
-    // so this is interesting. apparently only one! owner at a time, which means that
-    // you could trigger mutable stuff when the owner is some specific function scope, and NEVER
-    // outside of that. Could make for a very VERY safe system that can support consistency.
-    // also check pointers and dereferencing logic inside rust.
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    print_to_byte(&arr, 5);
+
+    // so new news. it looks like & allows you to "borrow" that reference for use in another scope.
 }
